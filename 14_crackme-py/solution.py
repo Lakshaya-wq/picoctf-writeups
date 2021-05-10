f7ea4f90e51d2a6a9d2b7bb54107de11aa21dabd
Lakshaya-wq <=>
2021-05-10 20:24:11 +0530

    add 14_crackme-py

---
+++ 14_crackme-py/solution.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+rot47_flag = "A:4@r%uL`M-^M0c0AbcM-MFE067d3eh2bN"
+
+alphabet = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ"+ \
+            "[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+
+
+
+def decode_rot47(string):
+
+    rotate_const = 47
+
+    decoded = ""
+
+    # decode loop
+    for c in string:
+        index = alphabet.find(c)
+        original_index = (index + rotate_const) % len(alphabet)
+        decoded = decoded + alphabet[original_index]
+
+    return decoded
+
+if __name__ == '__main__':
+	print(decode_rot47(rot47_flag))
